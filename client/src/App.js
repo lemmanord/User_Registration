dccb1d73ceda8161fd4b7ac907341430dba21ae2
Emman Tolosa <emmantolosa.stack@gmail.com>
2021-09-06 16:55:47 +0800

    logic for add delete update not yet tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AddForm from 'components/AddForm';
+import EditForm from 'components/EditForm';
+import UserList from 'components/UserList';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' component={UserList} />
+        <Route exact path='/edit/:id' component={EditForm} />
+        <Route exact path='/add' component={AddForm} />
+      </Switch>
+    </>
   );
 }
-
-export default App;
